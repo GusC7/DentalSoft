417383f08947a9c8c75e3ebc2c237be813ffbe74
Gustavo <gustavo.melgarejo@fceqyn.edu.ar>
2025-11-13 03:48:51 -0300

    Inicializando el proyecto

---
+++ dentalsoft/src/test/java/com/example/dentalsoft/DentalsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dentalsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
